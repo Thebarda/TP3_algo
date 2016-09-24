f5977ec1654bb42d2bfa69dfae12470cee585eb9
ThebardaPNK <tomdar87@outlook.com>
2016-09-24 10:35:36 +0200

    module fini et ajout des pages pour les procédures/fonctions

--- TP3/TP3/main.h
+++ TP3/TP3/main.h
@@ -4,4 +4,55 @@
 #include <iostream>
 using namespace std;
 
+typedef int Elt;
+
+struct Liste {
+	int nbElem;
+	int tailleTab;
+	Elt * tab;
+};
+
+//Procedure qui initialise une liste
+//Pré-condition
+//	liste nulle
+//Post-condition
+//	liste initialisé
+void init();
+
+//Procedure qui desinitialise une liste
+//Post-condition
+//	liste desinitialisé
+void desinit();
+
+//Procedure qui ajoute un élément à la liste en une position donnée
+//	li : la liste
+//	x : entier à ajouter
+//	pos : la position à laquelle on ajoute l'élément
+//Pré-condition
+//	0<=pos<=tailleTab
+//Post-condition
+//	élément ajouté à la liste à la position
+void adjElemList(Liste li, Elt x, int pos);
+
+//Procedure qui supprime un élément en une position donnée
+//	li : la liste
+//	pos : la position à laquelle on supprime l'élément
+//Pré-condition
+//	0<=pos<=tailleTab
+//	liste non vide
+//Post-condition
+//	élément supprimé de la liste
+void supElemList(Liste li, int pos);
+
+//Procedure qui affiche la liste
+//	li : la liste
+//Pré-condition
+//	Liste non vide
+void affList(Liste li);
+
+//Fonction qui récupère la longueur de la liste
+//	li : la liste
+//Retourne 0 si la liste est vide, nbElem sinon
+int longueurList(Liste li);
+
 #endif
